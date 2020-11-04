510bb89478dc1d57d7b5c06a804e0d0b2624e658
parasharbhatt <parasharbhatt@yahoo.com>
2020-11-03 23:12:33 -0500

    Initial implementation complete

--- search_dict.py
+++ search_dict.py
@@ -26,3 +26,68 @@ fp.close()
 
 
 #print(fl_content)
+
+# Below code loads json content into dictionary object using json.loads built-in method of json package
+d=json.loads(fl_content)
+print(f"Current different words in dictionary are: {len(d)} \n")
+
+#print(d)    
+
+#l=d["rain"]
+#print(l)
+
+    
+# Get word from user to search in dictionary
+
+search_word=input("Enter word to search in dictionary: ")
+
+print("\n\nResult: \n")
+
+# Get keys from dictionary
+k=d.keys()
+
+# store all keys of dictionary in lower case
+
+n_k=[]
+for x in k:
+    n_k.append(x.lower())
+#print(k)
+    
+
+
+def translate(word):
+    word=word.lower()
+       
+    if word.lower() in d:
+        meanings =  d[word.lower()] 
+    elif word.title() in d:
+        meanings =  d[word.title()] 
+    elif word.upper() in d:
+        meanings = d[word.upper()]
+    else:
+        if len( get_close_matches(word, d.keys(),n=3,cutoff=0.8)) > 0:
+            yn= input( "did you mean %s instead? Enter 'Y' OR 'N' " % get_close_matches( word, d.keys(),n=3,cutoff=0.8)[0])
+            yn=yn.upper()
+            if yn == "Y":
+                meanings =  get_close_matches( word, d.keys(),n=3,cutoff=0.8)[0]
+            elif yn== "N":
+                meanings="The word doesn't exist please double check it"
+            else:
+                meanings="We didn't understand your entry!"
+        else:
+              meanings=None
+     
+    return meanings
+
+result=translate(search_word)    
+           
+
+if result == None:
+    print("Can not display, Word does not exists in our dictionary ! ")
+else:
+    if type(result)==list:
+        for x in result:
+            print(x)
+    else:
+        print (result)
+    
